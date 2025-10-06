baed2ca86dd3d8a5ace24fb44d9f4b7d590d0259
mirkodgzconsulting <mirkodgzconsulting>
2025-10-06 10:45:45 +0200

    d

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -55,7 +55,7 @@ export const Modal: React.FC<ModalProps> = ({
     : "relative w-auto rounded-3xl bg-white  dark:bg-gray-900";
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center overflow-y-auto modal z-[999999]">
+    <div className="fixed inset-0 flex items-center justify-center p-4 modal z-[999999]">
       {!isFullscreen && (
         <div
           className="fixed inset-0 h-full w-full bg-gray-400/50 backdrop-blur-[32px] z-[999998]"
@@ -64,7 +64,7 @@ export const Modal: React.FC<ModalProps> = ({
       )}
       <div
         ref={modalRef}
-        className={`${contentClasses}  ${className}`}
+        className={`${contentClasses} ${className} relative z-[999999] max-h-[90vh] overflow-y-auto`}
         onClick={(e) => e.stopPropagation()}
       >
         {showCloseButton && (

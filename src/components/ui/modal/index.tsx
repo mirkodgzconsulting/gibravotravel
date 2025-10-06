19c57bc406ebf7dec30a6fe4892d0e296e134d6d
mirkodgzconsulting <mirkodgzconsulting>
2025-10-06 10:37:52 +0200

    r

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -55,10 +55,10 @@ export const Modal: React.FC<ModalProps> = ({
     : "relative w-auto rounded-3xl bg-white  dark:bg-gray-900";
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center overflow-y-auto modal z-99999">
+    <div className="fixed inset-0 flex items-center justify-center overflow-y-auto modal z-[999999]">
       {!isFullscreen && (
         <div
-          className="fixed inset-0 h-full w-full bg-gray-400/50 backdrop-blur-[32px]"
+          className="fixed inset-0 h-full w-full bg-gray-400/50 backdrop-blur-[32px] z-[999998]"
           onClick={onClose}
         ></div>
       )}
